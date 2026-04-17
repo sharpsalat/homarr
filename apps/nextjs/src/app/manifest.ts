3773b2aa82ee30704720fda23a2bb180637bd7c4
sharpsalat <deniska.xrenov23@yandex.ru>
2026-04-17 19:09:28 +0200

    crime

--- apps/nextjs/src/app/manifest.ts
+++ apps/nextjs/src/app/manifest.ts
@@ -14,19 +14,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/images/pwa/192.maskable.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "any",
-      },
-      {
-        src: "/images/pwa/192.maskable.png",
-        sizes: "192x192",
-        type: "image/png",
         purpose: "maskable",
-      },
-      {
-        src: "/images/pwa/512.maskable.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any",
       },
       {
         src: "/images/pwa/512.maskable.png",
